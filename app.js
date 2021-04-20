96185e29757a9b27bde0aae6617d4b58d828c808
mjbauka77 <mjbauka77@gmail.com>
2021-04-20 15:05:29 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is my DevOps demo MBauka!'));
+app.get('/', (req, res) => res.send('Me against the world MBauka!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
